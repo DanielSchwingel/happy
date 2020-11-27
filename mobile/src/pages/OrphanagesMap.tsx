2606fdf0bd780d6ce0038665e787058722f69817
unknown <danielfilipeschwingel@gmail.com>
2020-11-26 23:21:26 -0300

    Install library expo-image-picker from images

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 import mapMarker from '../images/map-marker.png';
@@ -26,7 +26,7 @@ export default function OrphanagesMap() {
 		navigation.navigate('SelectMapPosition');
 	}
 
-	useEffect(()=> {
+	useFocusEffect(()=> {
 		api.get('orphanages').then(response => {
 			setOrphanages(response.data);
 		})
