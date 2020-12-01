f06db38ffc3fb56b4bcfa3ea557319a453db314a
unknown <danielfilipeschwingel@gmail.com>
2020-11-30 23:20:59 -0300

    Finish create user route

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -1,15 +1,34 @@
 import { Request, Response } from 'express';
+import User from '../models/User';
+import { getRepository } from 'typeorm';
 import bcrypt from 'bcrypt';
+import * as Yup from 'yup';
 
 const saltRoudns = 10;
 
 export default {
    async create(request: Request, response: Response){
-      const { password } = request.body;
+      const { name, email, password } = request.body;
 
       const salt = bcrypt.genSaltSync(saltRoudns);
-      const passwordCrypt = bcrypt.hashSync(password, salt)
+      const passwordCrypt = bcrypt.hashSync(password, salt);
+      
+      const userRepository = getRepository(User);
 
-      return response.json({passwordCrypt});
+      const schema = Yup.object().shape({
+			name: Yup.string().required(),
+         email: Yup.string().required(),
+         password: Yup.string().required(),
+      });
+      
+      const data = { name, email, password: passwordCrypt }
+
+		await schema.validate(data, {
+			abortEarly: false,
+		});
+
+		const user = userRepository.create(data); 
+		await userRepository.save(user);
+		return response.status(201).json(user);
    }
 }
